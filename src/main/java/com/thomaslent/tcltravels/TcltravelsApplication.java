e85919ad81bbc4814c2542f194c6cb79ba21c0b2
Thomas Lent <911786+tlent@users.noreply.github.com>
2026-01-18 11:27:20 -0500

    Initial commit

---
+++ src/main/java/com/thomaslent/tcltravels/TcltravelsApplication.java
@@ -0,0 +1,13 @@
+package com.thomaslent.tcltravels;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TcltravelsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TcltravelsApplication.class, args);
+	}
+
+}

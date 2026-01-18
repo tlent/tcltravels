f3a55336035c55dbbbb5d3380e6f4aeaa7a07447
Thomas Lent <911786+tlent@users.noreply.github.com>
2026-01-18 13:25:04 -0500

    Fix application name casing

--- src/main/java/com/thomaslent/tcltravels/TcltravelsApplication.java
+++ src/main/java/com/thomaslent/tcltravels/TcltravelsApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TcltravelsApplication {
+public class TclTravelsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TcltravelsApplication.class, args);
+		SpringApplication.run(TclTravelsApplication.class, args);
 	}
 
 }

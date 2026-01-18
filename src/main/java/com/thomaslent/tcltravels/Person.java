b0e0daa9da40a9d2c28c6ee138fb72e0b22f1591
Thomas Lent <911786+tlent@users.noreply.github.com>
2026-01-18 13:58:39 -0500

    Add Person and User entities and repositories

---
+++ src/main/java/com/thomaslent/tcltravels/Person.java
@@ -0,0 +1,32 @@
+package com.thomaslent.tcltravels;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "persons")
+public class Person {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(name = "first_name", nullable = false)
+  private String firstName;
+
+  @Column(name = "last_name", nullable = false)
+  private String lastName;
+
+  private String address;
+  private String city;
+  private String state;
+
+  @Column(name = "zip_code", nullable = false)
+  private Integer zipCode;
+
+  @Column(nullable = false)
+  private String telephone;
+}
---
+++ src/main/java/com/thomaslent/tcltravels/User.java
@@ -0,0 +1,29 @@
+package com.thomaslent.tcltravels;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "app_users")
+public class User {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(unique = true, nullable = false)
+  private String username;
+
+  @Column(nullable = false)
+  private String password;
+
+  @OneToOne(cascade = CascadeType.ALL)
+  @JoinColumn(name = "person_id", referencedColumnName = "id")
+  private Person person;
+}
